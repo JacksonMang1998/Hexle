cb4d13d7daa0f1063617975df59c5bf71275df60
Jackson mang <113635656+JacksonMang1998@users.noreply.github.com>
2026-05-02 18:48:27 -0500

    Update index.js

--- hexle-backend-main/models/index.js
+++ hexle-backend-main/models/index.js
@@ -6,7 +6,11 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const db = {};
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false
+});
 
 fs.readdirSync(__dirname)
 	.filter((file) => {
